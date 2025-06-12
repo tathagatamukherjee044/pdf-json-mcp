4ee5d0508a695e4d0b475e7bf415cd112d889ece
kosac-achyut <43808123+kosac-achyut@users.noreply.github.com>
2025-06-12 15:05:48 +0530

    Added Embedder

---
+++ weavite.py
@@ -0,0 +1,60 @@
+import weaviate
+from weaviate.classes.init import Auth
+import os
+from dotenv import load_dotenv
+import requests,json
+
+load_dotenv()  # Load environment variables from .env file
+
+# Best practice: store your credentials in environment variables
+weaviate_url = os.getenv("WEAVIATE_URL")
+weaviate_api_key = os.getenv("WEAVIATE_API_KEY")
+
+
+client = weaviate.connect_to_weaviate_cloud(
+    cluster_url=weaviate_url,
+    auth_credentials=Auth.api_key(weaviate_api_key),
+)
+
+# way to inseter into DB 
+
+resp = requests.get(
+    "https://raw.githubusercontent.com/weaviate-tutorials/quickstart/main/data/jeopardy_tiny.json"
+)
+data = json.loads(resp.text)
+
+questions = client.collections.get("Question")
+
+with questions.batch.fixed_size(batch_size=200) as batch:
+    for d in data:
+        batch.add_object(
+            {
+                "answer": d["Answer"],
+                "question": d["Question"],
+                "category": d["Category"],
+            }
+        )
+        if batch.number_errors > 10:
+            print("Batch import stopped due to excessive errors.")
+            break
+
+failed_objects = questions.batch.failed_objects
+if failed_objects:
+    print(f"Number of failed imports: {len(failed_objects)}")
+    print(f"First failed object: {failed_objects[0]}")
+
+
+
+
+response = questions.query.near_text(
+    query="biology",
+    limit=2
+)
+
+for obj in response.objects:
+    print(json.dumps(obj.properties, indent=2))
+
+
+print(client.is_ready())  # Should print: `True`
+
+client.close()  # Free up resources
